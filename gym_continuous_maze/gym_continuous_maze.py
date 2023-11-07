e901e7469fa6d92c64f5cee09780f4182b78d433
KoOBaALT <luithardt.jo96@gmail.com>
2023-11-07 12:00:44 +0100

    Update gym_continuous_maze.py

--- gym_continuous_maze/gym_continuous_maze.py
+++ gym_continuous_maze/gym_continuous_maze.py
@@ -140,7 +140,7 @@ class ContinuousMaze(gym.Env):
         terminated = False
         truncated = False
         info = {}
-        return observation, reward, terminated, truncated, info
+        return observation, reward, terminated, info
 
     def reset(self, seed: int | None = None, options: dict = {}) -> np.ndarray:
         self.pos = np.zeros(2, dtype=np.float32)
